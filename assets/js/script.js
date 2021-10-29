8d47ab1a04e6d777394a51f441020b2c35de77a3
Edison Simondet <edison.simondet@gmail.com>
2021-10-28 21:09:06 -0500

    enhancements to functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// regex to confirm the presence of lowercase characters in our password if desired
+let lowercaseRegex =  /.*[a-z]+.*/;
+
+// regex to confirm the presence of uppercase characters in our password if desired
+let uppercaseRegex =  /.*[A-Z]+.*/;
+
+// regex to confirm the presence of numeric characters in our password if desired
+let numericRegex =  /.*[0-9]+.*/;
+
+// regex to confirm the presence of special characters in our password if desired
+let specialRegex =  /.*[ !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]+.*/;
+
 // constant string for lowercase letters
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 
@@ -15,7 +27,7 @@ var randomNumber = function (min, max) {
   var value = Math.floor(Math.random() * (max - min) + min);
 
   return value;
-};
+}
 
 // prompt use with question and confirm user entry as yes or no
 var confirmEntry = function (userPrompt) {
@@ -26,11 +38,22 @@ var confirmEntry = function (userPrompt) {
   return userResponse;
 }
 
+//function to build password
+var buildPassword = function(referenceString, passwordLength) {
+  var generatedPassword = "";
+
+  while (generatedPassword.length < passwordLength) {
+    generatedPassword += referenceString.charAt(randomNumber(0, referenceString.length));
+  }
+
+  return generatedPassword;
+}
+
 // begin password component picker
 var generatePassword = function () {
   var passwordLength = "";
 
-  while ((passwordLength === "" || passwordLength === null) || (passwordLength < 8 || passwordLength > 128)) {
+  while (passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     passwordLength = prompt("Please enter a number 8 through 128");
   }
 
@@ -42,6 +65,11 @@ var generatePassword = function () {
 
   var passwordSpecial = confirmEntry("Would you like this password to include special characters? Y/N");
 
+  //If user chooses a valid password length but does not include any characters, display message in password textbox
+  if (passwordLowercase === "N" && passwordUppercase === "N" && passwordNumeric === "N" && passwordSpecial === "N") {
+    return "You have to pick at least *one* of the character types";
+  }
+
   var generatedPassword = "";
 
   var referenceString = "";
@@ -51,14 +79,25 @@ var generatePassword = function () {
   if (passwordNumeric === "Y") referenceString += numbers;
   if (passwordSpecial === "Y") referenceString += specialCharacters;
 
-  while (generatedPassword.length < passwordLength) {
-
-    generatedPassword += referenceString.charAt(randomNumber(0, referenceString.length));
+  while (!passwordValidate(passwordLowercase, passwordUppercase, passwordNumeric, passwordSpecial, generatedPassword)) {
+    generatedPassword = "";
+    generatedPassword = buildPassword(referenceString, passwordLength);
   }
 
   return generatedPassword;
 }
 
+//function to validate the presence of desired characters
+var passwordValidate = function(passwordLowercase, passwordUppercase, passwordNumeric, passwordSpecial, generatedPassword) {
+  var isValid = true;
+
+  if (passwordLowercase === "Y" && !lowercaseRegex.test(generatedPassword)) isValid = false;
+  if (passwordUppercase === "Y" && !uppercaseRegex.test(generatedPassword)) isValid = false;
+  if (passwordNumeric === "Y" && !numericRegex.test(generatedPassword)) isValid = false;
+  if (passwordSpecial === "Y" && !specialRegex.test(generatedPassword)) isValid = false;
+
+  return isValid;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
