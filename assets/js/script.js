b0d3774bca8aeb6725ba3cff5f74470223a64725
Edison Simondet <edison.simondet@gmail.com>
2021-10-28 20:28:14 -0500

    Minimum viable product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // constant string for lowercase letters
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"; 
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 
 // constant string for uppercase letters
-const uppercaseLetters = lowercaseLetters.toUpperCase();
+const uppercaseLetters = lowercaseLetters.toUpperCase(); //function to convert string to uppercase
 
 // constant string for numbers
 const numbers = "0123456789";
@@ -10,17 +10,24 @@ const numbers = "0123456789";
 // constant string for special characters
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min) + min);
+
+  return value;
+};
+
 // prompt use with question and confirm user entry as yes or no
 var confirmEntry = function (userPrompt) {
   var userResponse = "";
   while ((userResponse === "" || userResponse === null) || (userResponse != "Y" && userResponse != "N")) {
-    userResponse = prompt(userPrompt).toUpperCase(); //function to convert string to uppercase
+    userResponse = prompt(userPrompt).toUpperCase();
   }
   return userResponse;
 }
 
 // begin password component picker
-var passwordStart = function () {
+var generatePassword = function () {
   var passwordLength = "";
 
   while ((passwordLength === "" || passwordLength === null) || (passwordLength < 8 || passwordLength > 128)) {
@@ -34,8 +41,23 @@ var passwordStart = function () {
   var passwordNumeric = confirmEntry("Would you like this password to include numbers? Y/N");
 
   var passwordSpecial = confirmEntry("Would you like this password to include special characters? Y/N");
-}
 
+  var generatedPassword = "";
+
+  var referenceString = "";
+
+  if (passwordLowercase === "Y") referenceString += lowercaseLetters;
+  if (passwordUppercase === "Y") referenceString += uppercaseLetters;
+  if (passwordNumeric === "Y") referenceString += numbers;
+  if (passwordSpecial === "Y") referenceString += specialCharacters;
+
+  while (generatedPassword.length < passwordLength) {
+
+    generatedPassword += referenceString.charAt(randomNumber(0, referenceString.length));
+  }
+
+  return generatedPassword;
+}
 
 
 // Get references to the #generate element
